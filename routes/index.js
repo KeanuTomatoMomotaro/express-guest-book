e5eaf6dd08cc59be0d66100297ed9605326b8827
Keanu Nurherbyanto <keanu.razgriz@gmail.com>
2018-03-11 17:23:06 +0700

    can get all guest comments

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const mysql = require('mysql');
+const dbconfig = require('../config/db');
+const connection = mysql.createConnection(dbconfig.connection);
+
+connection.query('USE ' + dbconfig.database);
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/get-all-guest-comments', function(req, res, next) {
+  connection.query(`select * from guest_comments`, function(err, rows){
+    if(err){
+      console.log(err,"ERROR")
+    }else{
+      res.render('all-guest-comments', { 
+        title: "Keanu's Guestbook",
+        guest_comments: rows
+       });      
+    }
+  })
 });
 
 module.exports = router;
